300b4fc047d9a9031e0af0ead5aba76764d05503
Kelly McLaughlin <kelly@basho.com>
2011-02-16 09:33:48 -0700

    Load the otp mib before checking the status of riak_kv_stat

--- src/riak_snmp_app.erl
+++ src/riak_snmp_app.erl
@@ -9,10 +9,10 @@
 %% @doc The application:start callback for riak.
 %%      Arguments are ignored as all configuration is done via the erlenv file.
 start(_Type, _StartArgs) ->
+    ok = otp_mib:load(snmp_master_agent),
     case application:get_env(riak_kv, riak_kv_stat) of
         {ok, true} ->       
             riak_core_util:start_app_deps(riak_snmp),
-            ok = otp_mib:load(snmp_master_agent),    
             riak_snmp_sup:start_link();
         _ ->
             {ok, self()}
